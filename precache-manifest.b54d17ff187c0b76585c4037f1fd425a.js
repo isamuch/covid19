653da2d669912d399d576b17e4f39264bb5263ff
samuch <samuch.work@gmail.com>
2020-04-25 19:39:25 +0700

    first commit

---
+++ precache-manifest.b54d17ff187c0b76585c4037f1fd425a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404a48525b3720b28c0e7b0cb08e3897",
+    "url": "/index.html"
+  },
+  {
+    "revision": "793f5793029ee8c27746",
+    "url": "/static/css/main.b42fc82c.chunk.css"
+  },
+  {
+    "revision": "a1d0803c8122afdf0ff9",
+    "url": "/static/js/2.8a8f1f25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a8f1f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "793f5793029ee8c27746",
+    "url": "/static/js/main.eee5d17a.chunk.js"
+  },
+  {
+    "revision": "e89038fb99fbf478b8ae",
+    "url": "/static/js/runtime-main.7c4d37a9.js"
+  },
+  {
+    "revision": "b4946b34e9acb1ce0ae7220df01dce9b",
+    "url": "/static/media/death.b4946b34.svg"
+  },
+  {
+    "revision": "5c310782cbb13e26a7044361622d5fca",
+    "url": "/static/media/fever.5c310782.svg"
+  },
+  {
+    "revision": "3fcab48b4c1913e7fd1ccd82870e7268",
+    "url": "/static/media/strength.3fcab48b.svg"
+  },
+  {
+    "revision": "c72046a7e3d40c8e7d0018ef05ca6a0e",
+    "url": "/static/media/thailand.c72046a7.svg"
+  },
+  {
+    "revision": "d29cc4fb8f65301d991ea4a1172d747b",
+    "url": "/static/media/virus.d29cc4fb.svg"
+  }
+]);
